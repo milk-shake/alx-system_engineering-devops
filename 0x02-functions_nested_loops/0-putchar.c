581789455a7c525e617e4c648123e78d401e8605
Njuguna Gikenye <joegikenye@gmail.com>
2022-07-05 11:31:45 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char *sh = "main";
+while (*sh)
+{
+_putchar
+sh++;
+}
+_putchar
+sh++;
+}
+_putchar('\n');
+return (0);
+}
