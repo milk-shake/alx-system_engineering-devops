1a76bc1abfc1be89304718728fe11db1e6a03413
Njuguna Gikenye <joegikenye@gmail.com>
2022-07-06 06:37:32 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * main - check the code
+ * main - function to print
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
-char *sh = "main";
-while (*sh)
-{
-_putchar
-sh++;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
-_putchar
-sh++;
-}
-_putchar("\n");
-return (0);
-}
+
+
